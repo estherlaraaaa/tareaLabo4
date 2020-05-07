fd2a7e35fb439b73ede36aa5d0e0b9ba12f7d194
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2020-05-07 13:44:50 -0600

    terminando el labo, falta css para tarea

---
+++ tareaLabo4/src/main/java/com/uca/capas/domain/Robot.java
@@ -0,0 +1,23 @@
+package com.uca.capas.domain;
+
+import javax.validation.constraints.AssertTrue;
+
+public class Robot {
+	
+	@AssertTrue(message="Ups no puedes agreagr producto porque eres un robot, favor hacer click en regresar")
+	private Boolean soyRobot;
+
+	public Robot() {
+		super();
+	}
+
+	public Boolean getSoyRobot() {
+		return soyRobot;
+	}
+
+	public void setSoyRobot(Boolean soyRobot) {
+		this.soyRobot = soyRobot;
+	}
+	
+	
+}
